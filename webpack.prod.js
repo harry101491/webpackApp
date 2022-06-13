f3ef5c2f4ec5e83264b160d6e1a31cb883fc63f2
harry101491 <hp1014@nyu.edu>
2022-06-12 22:03:46 -0700

    feat(adding html-loader and clean-webpack-plugin)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const common = require('./webpack.common');
 const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -8,4 +9,5 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
   },
+  plugins: [new CleanWebpackPlugin()],
 });
